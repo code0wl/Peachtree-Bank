f92b8a1d8a750761bf18bd3ccc671caff52a3777
Mohammad Sirajuddin Rayyan <mds0060@gmail.com>
2021-02-05 23:25:14 +0100

    Logo, Header and Footers added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {BbUIModule} from './shared/bb-ui.module';
 import {AppRoutingModule} from "./app-routing.module";
 import {BrowserModule} from "@angular/platform-browser";
 import {HeaderComponent} from "./shared/components/header/header.component";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     AppRoutingModule,
     BbUIModule],
   providers: [],
