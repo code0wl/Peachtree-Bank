11945887ef82689e427a17cf1f2304b7b23b3a6c
Mohammad Sirajuddin Rayyan <mds0060@gmail.com>
2021-02-05 22:08:56 +0100

    Header Component created and Directory changed for main & _vars scss files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
+import {BbUIModule} from './shared/bb-ui.module';
+import {AppRoutingModule} from "./app-routing.module";
+import {BrowserModule} from "@angular/platform-browser";
+import {HeaderComponent} from "./shared/components/header/header.component";
 
 @NgModule({
-  declarations: [],
-  imports: [],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BbUIModule],
   providers: [],
   bootstrap: [AppComponent]
 })
