a3a07fca9f27a842146a17e72fc7c5f9fbe2edf7
Mohammad Sirajuddin Rayyan <mds0060@gmail.com>
2021-02-07 23:53:16 +0100

    Tech Spike: specs for filter pipe added

--- src/app/shared/pipes/sort-by-date.pipe.spec.ts
+++ src/app/shared/pipes/sort-by-date.pipe.spec.ts
@@ -1,7 +1,7 @@
 import {SortByDatePipe} from './sort-by-date.pipe';
-import {TransactionData} from "../models/transaction-data.model";
+import testData from '../../core/mock-data/mock-test-data';
 
-fdescribe('SortByDatePipe', () => {
+describe('SortByDatePipe', () => {
   let pipe: SortByDatePipe;
 
   beforeEach(() => {
@@ -26,58 +26,4 @@ fdescribe('SortByDatePipe', () => {
     const result = pipe.transform(testData, 'asc');
     expect(result[0].merchant.name).toBe('7-Eleven');
   });
-
-
-  const testData = [{
-    categoryCode: '#c12020',
-    dates: {
-      valueDate: 1600370800000
-    },
-    transaction: {
-      amountCurrency: {
-        amount: '22.10',
-        currencyCode: 'EUR'
-      },
-      type: 'Online Transfer',
-      creditDebitIndicator: 'DBIT'
-    },
-    merchant: {
-      name: 'Amazon Online Store',
-      accountNumber: 'SI64397745065188826'
-    }
-  }, {
-    categoryCode: '#c89616',
-    dates: {
-      valueDate: 1600214400000
-    },
-    transaction: {
-      amountCurrency: {
-        amount: '46.25',
-        currencyCode: 'EUR'
-      },
-      type: 'Card Payment',
-      creditDebitIndicator: 'DBIT'
-    },
-    merchant: {
-      name: '7-Eleven',
-      accountNumber: 'SI64397745065188826'
-    }
-  }, {
-    categoryCode: '#e25a2c',
-    dates: {
-      valueDate: 1602633600000
-    },
-    transaction: {
-      amountCurrency: {
-        amount: '19.72',
-        currencyCode: 'EUR'
-      },
-      type: 'Online Transfer',
-      creditDebitIndicator: 'DBIT'
-    },
-    merchant: {
-      name: 'H&M Online Store',
-      accountNumber: 'SI64397745065188826'
-    }
-  }] as TransactionData[];
 });
